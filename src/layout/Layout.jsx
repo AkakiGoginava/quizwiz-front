fded41c5201442863db347836f9d58ee006f6ce5
AkakiGoginava <akakigoginava2808@gmail.com>
2025-06-05 19:40:21 +0400

    chore: add landing page

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -7,7 +7,7 @@ function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 relative">{children}</main>
       <Footer />
     </div>
   );
---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logoBig from "@/assets/images/logoBig.png";
+import landingImg from "@/assets/images/landingImg.png";
+
+function Landing() {
+  return (
+    <>
+      <img className="h-75 w-165 mt-7 mb-28 ml-24" src={logoBig} />
+
+      <div className="flex flex-col justify-center w-full bg-[#FF5613] text-white font-['Raleway'] pl-24 pt-8 pb-14.5">
+        <p className="text-[5rem]">200+</p>
+        <p className="text-[3rem]">Quiz games</p>
+      </div>
+
+      <div className="flex flex-col justify-center w-full bg-[#4B69FD] text-white font-['Raleway'] pl-24 pb-9.5">
+        <p className="text-[5rem]">25+</p>
+        <p className="text-[3rem]">Different genre</p>
+      </div>
+
+      <img className="absolute bottom-0 right-0" src={landingImg} />
+    </>
+  );
+}
+
+export default Landing;
