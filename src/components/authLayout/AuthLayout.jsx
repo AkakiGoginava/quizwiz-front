ff0e13137a29b42d9273a22384b0304f5bb8106c
AkakiGoginava <akakigoginava2808@gmail.com>
2025-06-07 19:23:11 +0400

    chore: add layout component for auth pages

---
+++ src/components/authLayout/AuthLayout.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import logo from "@/assets/images/logo.png";
+import arrowIcon from "@/assets/images/arrow-icon.png";
+import { Link, useNavigate } from "react-router-dom";
+
+function AuthLayout({ coverImg, children }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex gap-16.5">
+      <Link to="/" className="absolute top-10 left-10">
+        <img src={logo} alt="logo" />
+      </Link>
+
+      <img
+        src={coverImg}
+        alt="cover image"
+        className="w-auto h-auto max-w-none"
+      />
+
+      <div className="flex flex-col">
+        <button
+          onClick={() => navigate(-1)}
+          type="button"
+          className="flex gap-2 mb-12 mt-11 font-medium text-[#667085] items-center hover:cursor-pointer hover:opacity-85"
+        >
+          <img src={arrowIcon} alt="arrow icon" />
+          Back
+        </button>
+        <div className="pl-6">{children}</div>
+      </div>
+    </div>
+  );
+}
+AuthLayout.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthLayout;
